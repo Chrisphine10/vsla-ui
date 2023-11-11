ff89987aacb04672aec54a3bc57247fbbcff8b01
Chrisphine10 <ondiekchrisphine@gmail.com>
2023-11-11 19:08:43 +0300

    all

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,5 @@
 export const constants = {
-    API_URL_V1: "https://vsla.cc/api/",
-    API_URL_V2: "https://vsla.cc/api/v1/",
-    API_AUTH_URL: "https://vsla.cc/api/",
+    API_URL_V1: "http://54.235.172.109:443/api/",
+    API_URL_V2: "http://54.235.172.109:443/v1/",
+    API_AUTH_URL: "http://54.235.172.109:443/api/",
 };
