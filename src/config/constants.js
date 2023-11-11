90bb0ad20682da596d3fbbca8c1ad6aa18197d2d
Chrisphine10 <ondiekchrisphine@gmail.com>
2023-11-11 19:09:56 +0300

    all

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,5 @@
 export const constants = {
-    API_URL_V1: "http://54.235.172.109:443/api/",
-    API_URL_V2: "http://54.235.172.109:443/v1/",
-    API_AUTH_URL: "http://54.235.172.109:443/api/",
+    API_URL_V1: "https://54.235.172.109:443/api/",
+    API_URL_V2: "https://54.235.172.109:443/v1/",
+    API_AUTH_URL: "https://54.235.172.109:443/api/",
 };
